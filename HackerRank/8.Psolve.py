e247bf5bb3b28e9fc3c500a55e33f7713e2add7c
unknown <masudcse74@gmail.com>
2023-02-09 10:04:56 +0600

    Solve

--- HackerRank/8.Psolve.py
+++ HackerRank/8.Psolve.py
@@ -1,11 +1,11 @@
 # List Comprehensions
 
-# Let's learn about list comprehensions! You are given three integers X, Y and Z representing the dimensions of a cuboid along with an
+# Let's learn about list comprehensions! You are given three integers x, y and z representing the dimensions of a cuboid along with an
 # integer N. You have to print a list of all possible coordinates given by (i, j, k) on a 3D grid where the sum of i + j + k  is not
-# equal to N. Here, 0 <= i <= X; 0 <= j <= Y; 0 <= k <= Z 
+# equal to n. Here, 0 <= i <= x; 0 <= j <= y; 0 <= k <= z
 
 # Input Format
-# Four integers X, Y, Z and N each on four separate lines, respectively.
+# Four integers x, y, z and N each on four separate lines, respectively.
 
 # Constraints
 # Print the list in lexicographic increasing order.
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     y = int(input())
     z = int(input())
     n = int(input())
-    myList = list()
+    # myList = list()
     # for i in range(x+1):
     #     for j in range(y+1):
     #         for k in range(z+1):
