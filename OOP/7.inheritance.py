7ba56e4e3fe322783942912393e70ef26b71802a
mranapy <masudcse74@gmail.com>
2024-10-20 23:53:16 +0600

    inheritance

--- OOP/7.inheritance.py
+++ OOP/7.inheritance.py
@@ -1,11 +1,11 @@
 class Vehicle():
     """Base class for all vehicle"""
 
-    def __init__(self, name, manufacturer, color, year):
-        self.name = name
-        self.manufacturer = manufacturer
-        self.color = color
-        self.year = year
+    def __init__(selfm, name, manufacturer, color, year):
+        selfm.name = name
+        selfm.manufacturer = manufacturer
+        selfm.color = color
+        selfm.year = year
 
     def drive(self):
         print("Driving >>> ", self.manufacturer, self.name, self.color)
@@ -59,3 +59,57 @@ if __name__ == "__main__":
     mc.drive()
     print("\n")
     mc.MC_details()
+# ----------------------------------------------------------------------
+
+class Dancer:
+    def __init__(self, style):
+        self.style = style
+
+    def dance(self):
+        print(f'Dancing in {self.style}')
+
+
+class Singer:
+    def __init__(self, genre):
+        self.genre = genre
+
+    def sing(self):
+        print(f'Singing {self.genre} music')
+
+
+class Artist:
+    def __init__(self, painting_material):
+        self.painting_material = painting_material
+
+    def paint(self):
+        print(f'Painting with {self.painting_material}')
+
+
+class SuperHuman(Dancer, Singer, Artist):
+    def __init__(self, style, genre, paint_materials, sport):
+        Dancer.__init__(self, style)
+        Singer.__init__(self, genre)
+        Artist.__init__(self, paint_materials)
+        self.sport = sport
+
+    def play_sport(self):
+        print(f'Playing {self.sport}')
+
+    # Overriding the Dance method of Dancer class
+    def dance(self, competition="Fresher's Cup"):
+        print(f'Dancing with my own {self.style} in the competition {competition}')
+
+
+s_human = SuperHuman('Hip-Hop', 'Classical', 'Acrylic', 'Football')
+# print(s_human.style)
+# print(s_human.genre)
+print(s_human.painting_material)
+# print(s_human.sport)
+s_human.dance()
+s_human.sing()
+# s_human.paint()
+s_human.play_sport()
+
+# print(help(SuperHuman))
+
+
