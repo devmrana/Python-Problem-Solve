01a67e20daf9b1d9ffd6ee28cbbb2127a7f2dfbd
mranapy <masudcse74@gmail.com>
2024-11-09 12:48:16 +0600

    Pattern

---
+++ pattern.py
@@ -0,0 +1,20 @@
+""""""
+def pattern(n):
+    for i in range(0,n):
+        for j in range(0,i+1):
+            print("*", end=" ")
+        print("\r")
+# pattern(6)
+
+def pattern(n):
+    k = 2 * n - 2
+    for i in range(0,n):
+        for j in range(0,k):
+            print(end=" ")
+        k = k - 2
+        for j in range(0,i+1):
+            print("* ", end="")
+        print("\r")
+
+pattern(6)
+
